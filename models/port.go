8a62e6ebd5856c89acb4a646734b31a51b084d29
Xavier Canal i Masjuan <xeviknal@gmail.com>
2018-02-22 13:28:34 +0100

    Add Endpoints to Service Details

--- models/port.go
+++ models/port.go
@@ -2,26 +2,41 @@ package models
 
 import "k8s.io/api/core/v1"
 
+type Ports []Port
 type Port struct {
 	Name     string `json:"name"`
 	Protocol string `json:"protocol"`
 	Port     int32  `json:"port"`
 }
 
-func CastPortCollection(ps []v1.ServicePort) []Port {
+func CastServicePortCollection(ps []v1.ServicePort) []Port {
 	ports := make([]Port, len(ps))
 	for i, servicePort := range ps {
-		ports[i] = CastPort(servicePort)
+		ports[i] = CastServicePort(servicePort)
 	}
 
 	return ports
 }
 
-func CastPort(p v1.ServicePort) Port {
+func CastServicePort(p v1.ServicePort) Port {
 	port := Port{}
 	port.Name = p.Name
 	port.Protocol = string(p.Protocol)
 	port.Port = p.Port
 
 	return port
+}
+
+func (ports *Ports) ParseEndpointPorts(ps []v1.EndpointPort) {
+	for _, endpointPort := range ps {
+		port := Port{}
+		port.ParseEndpointPort(endpointPort)
+		*ports = append(*ports, port)
+	}
+}
+
+func (port *Port) ParseEndpointPort(p v1.EndpointPort) {
+	port.Name = p.Name
+	port.Protocol = string(p.Protocol)
+	port.Port = p.Port
 }
