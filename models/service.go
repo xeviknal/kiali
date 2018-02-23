d1416ea0d7a2e6bbc971565cb233f1817bcd69ff
Xavier Canal i Masjuan <xeviknal@gmail.com>
2018-02-23 12:18:47 +0100

    Adding Dependencies to Service Details

--- models/service.go
+++ models/service.go
@@ -2,6 +2,8 @@ package models
 
 import (
 	"github.com/swift-sunshine/swscore/kubernetes"
+	"github.com/swift-sunshine/swscore/prometheus"
+
 	"k8s.io/api/core/v1"
 )
 
@@ -15,15 +17,16 @@ type ServiceList struct {
 }
 
 type Service struct {
-	Name       string            `json:"name"`
-	Namespace  Namespace         `json:"namespace"`
-	Labels     map[string]string `json:"labels"`
-	Type       string            `json:"type"`
-	Ip         string            `json:"ip"`
-	Ports      Ports             `json:"ports"`
-	Endpoints  Endpoints         `json:"endpoints"`
-	Pods       Pods              `json:"pods"`
-	RouteRules RouteRules        `json:"route_rules"`
+	Name         string            `json:"name"`
+	Namespace    Namespace         `json:"namespace"`
+	Labels       map[string]string `json:"labels"`
+	Type         string            `json:"type"`
+	Ip           string            `json:"ip"`
+	Ports        Ports             `json:"ports"`
+	Endpoints    Endpoints         `json:"endpoints"`
+	Pods         Pods              `json:"pods"`
+	RouteRules   RouteRules        `json:"route_rules"`
+	Dependencies map[string]string `json:"dependencies"`
 }
 
 func GetServicesByNamespace(namespaceName string) ([]ServiceOverview, error) {
@@ -46,6 +49,11 @@ func GetServiceDetails(namespaceName, serviceName string) (*Service, error) {
 		return nil, err
 	}
 
+	prometheusClient, err := prometheus.NewClient()
+	if err != nil {
+		return nil, err
+	}
+
 	serviceDetails, err := istioClient.GetServiceDetails(namespaceName, serviceName)
 	if err != nil {
 		return nil, err
@@ -56,7 +64,12 @@ func GetServiceDetails(namespaceName, serviceName string) (*Service, error) {
 		return nil, err
 	}
 
-	return CastService(serviceDetails, istioDetails), nil
+	incomeServices, err := prometheusClient.GetSourceServices(namespaceName, serviceName)
+	if err != nil {
+		return nil, err
+	}
+
+	return CastService(serviceDetails, istioDetails, incomeServices), nil
 }
 
 func CastServiceOverviewCollection(sl *v1.ServiceList) []ServiceOverview {
@@ -75,13 +88,14 @@ func CastServiceOverview(s v1.Service) ServiceOverview {
 	return service
 }
 
-func CastService(s *kubernetes.ServiceDetails, i *kubernetes.IstioDetails) *Service {
+func CastService(s *kubernetes.ServiceDetails, i *kubernetes.IstioDetails, dependencies map[string]string) *Service {
 	service := &Service{}
 	service.Name = s.Service.Name
 	service.Namespace = Namespace{s.Service.Namespace}
 	service.Labels = s.Service.Labels
 	service.Type = string(s.Service.Spec.Type)
 	service.Ip = s.Service.Spec.ClusterIP
+	service.Dependencies = dependencies
 	(&service.Ports).Parse(s.Service.Spec.Ports)
 	(&service.Endpoints).Parse(s.Endpoints)
 	(&service.Pods).Parse(s.Pods)
